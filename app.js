b70dbf8c6e428aaeb2c118da86de0823654037da
farid64 <farid.sobhany@gmail.com>
2017-10-03 20:39:53 -0700

    edit redirect

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyAE3iWo1CD2XmAWvffnBSyP8kFUbLOjR3c",
     authDomain: "singin-ba4f0.firebaseapp.com",
     databaseURL: "https://singin-ba4f0.firebaseio.com",
     projectId: "singin-ba4f0",
     storageBucket: "singin-ba4f0.appspot.com",
     messagingSenderId: "412372715700"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  database = firebase.database();
+database = firebase.database();
 
-  function emailDisguise(email2){
+function emailDisguise(email2) {
     var em = email2;
-    em = em.replace("." , ",");
+    em = em.replace(".", ",");
     return em;
-  };
+};
 
-  function loadUser(){
+function loadUser() {
 
     var user = firebase.auth().currentUser;
 
-    if(user != null){
-      $("#content").html("<h1>" + user.email + " : " + user.displayName + "</h1>");
+    if (user != null) {
+        $("#content").html("<h1>" + user.email + " : " + user.displayName + "</h1>");
     }
-  }
+}
 
-  // console.log(emailDisguise("salam.return"));
+// console.log(emailDisguise("salam.return"));
 
-  var txtEmail = $("#txtEmail");
-  var txtPassword = $("#txtPassword");
-  // var btnLogin = $("#btnLogin");
-  // var btnSignUP = $("#btnSignUP");
-  // var btnLogout = $("#btnLogout");
+var txtEmail = $("#txtEmail");
+var txtPassword = $("#txtPassword");
+// var btnLogin = $("#btnLogin");
+// var btnSignUP = $("#btnSignUP");
+// var btnLogout = $("#btnLogout");
 
-  $("#btnLogin").on('click' , function(){
+$("#btnLogin").on('click', function() {
+  
     console.log("olleh");
     var email = txtEmail.val();
     var pass = txtPassword.val();
@@ -43,9 +43,9 @@
     // sing in
     var promise = auth.signInWithEmailAndPassword(email, pass);
 
-  })
+})
 
-  $("#btnSignUp").on('click' , function(){
+$("#btnSignUp").on('click', function() {
     // console.log("hello");
     var email = txtEmail.val();
     var pass = txtPassword.val();
@@ -57,67 +57,72 @@
     var promise = auth.createUserWithEmailAndPassword(email, pass);
     // create a new Node
     database.ref('/Users/' + emailDisguise(window.email)).set({
-      'email' : email,
-      'address' : "18440 hatteras st"
+        'email': email,
+        'address': "18440 hatteras st"
     })
-  })
+})
 
-  firebase.auth().onAuthStateChanged( function(firebaseUser){
+firebase.auth().onAuthStateChanged(function(firebaseUser) {
 
-    if(firebaseUser){
-      // console.log(firebaseUser);
-      $("#btnLogout").removeClass("hide");
-      $("#btnLogout").addClass("show");
-          loadUser();
-    }else{
-      console.log('not logged in');
-      $("#btnLogout").removeClass("show");
-      $("#btnLogout").addClass("hide");
+    if (firebaseUser) {
+        // console.log(firebaseUser);
+        $("#btnLogout").removeClass("hide");
+        $("#btnLogout").addClass("show");
+        loadUser();
+    } else {
+        console.log('not logged in');
+        $("#btnLogout").removeClass("show");
+        $("#btnLogout").addClass("hide");
     }
-  });
+});
 
 
-  $("#btnLogout").on('click' , function(){
+$("#btnLogout").on('click', function() {
     firebase.auth().signOut();
     $("#content").empty();
-  })
+})
 
-  $("#enterAddress").on('click' , function(){
+$("#enterAddress").on('click', function() {
     var emailz = firebase.auth().currentUser.email;
 
     var addr = $("#address").val().trim();
-    emailz = emailz.replace("." , ",");
+    emailz = emailz.replace(".", ",");
 
     database.ref('/Users/' + emailz).update({
-      'address' : addr
+        'address': addr
     })
-  })
+})
 
 
-  //This section is for facebook login
+//This section is for facebook login
 
-  $("#btnFacebook").on('click' , function(){
+$("#btnFacebook").on('click', function() {
+
     console.log("Facebook");
 
-  var provider = firebase.auth.FacebookAuthProvider();
+    var provider = new firebase.auth.FacebookAuthProvider();
 
-  provider.addScope('user_friends');
+    // provider.addScope('user_friends');
 
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+    firebase.auth().getRedirectResult().then(function(result) {
+
+        if (result.credential) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
 
 });
