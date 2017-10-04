f8f7153acd304ca8099b150745fa13a2d0136b8f
farid64 <farid.sobhany@gmail.com>
2017-10-03 20:19:56 -0700

    added the facebook auth

--- app.js
+++ app.js
@@ -94,5 +94,29 @@
   })
 
 
-  //e86edc7823204eca23eaaffa56eb0bcb //facebook secret
-  //154400321824663 app id
+  //This section is for facebook login
+
+  $("#btnFacebook").on('click' , function(){
+
+  var provider = firebase.auth.FacebookAuthProvider();
+
+  provider.addScope('user_friends');
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+});
